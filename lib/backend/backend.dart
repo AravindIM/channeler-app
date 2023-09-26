b1592191625639b23f1170f6898c6560983a2b6a
Aravind I M <aravindim@yahoo.com>
2023-09-27 01:54:27 +0530

    feat(backend): new algorithm to cache and generate list of posts

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
+
 import 'package:channeler/backend/api_endpoint.dart';
 import 'package:channeler/backend/board.dart';
-import 'package:channeler/backend/thread.dart';
+import 'package:channeler/backend/post.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class Backend {
   List<Board> boards = [];
+  List<dynamic> pageList = [];
   final ApiEndpoint api = ApiEndpoint.for4chan();
 
   Future<List<Board>> fetchBoards() async {
@@ -30,33 +33,46 @@ class Backend {
     return board;
   }
 
-  Future<List<Thread>> fetchThread(String boardName, String id) async {
-    List<Thread> threads = [];
+  Future<Post> fetchPost(String boardName, int id) async {
     try {
       final response = await http.get(api.getThreadUri(boardName, id),
           headers: {'Accept': 'application/json'});
       if (response.statusCode == 200) {
-        threads = await compute(parseThreads, response.body);
+        final thread = jsonDecode(response.body) as Map<String, dynamic>;
+        return compute(Post.fromThreadJson, thread);
       } else {
         return Future.error('Failed to fetch thread');
       }
-      return threads;
     } catch (e) {
       return Future.error(e.toString());
     }
   }
 
-  Future<List<Thread>> fetchPage(String boardName, int page) async {
-    List<Thread> threads = [];
+  List<int> getThreadList(List<dynamic> pages, int pageNum) {
+    final page = pages[pageNum] as Map<String, dynamic>;
+    final threadList = page['threads'] as List<dynamic>;
+
+    return threadList.map((json) {
+      final entry = json as Map<String, dynamic>;
+      return entry['no'] as int;
+    }).toList();
+  }
+
+  Future<List<Post>> fetchPage(String boardName, int pageNum) async {
     try {
-      final response = await http.get(api.getPageUri(boardName, page),
-          headers: {'Accept': 'application/json'});
-      if (response.statusCode == 200) {
-        threads = await compute(parseThreads, response.body);
-      } else {
-        return Future.error('Failed to fetch page');
+      if (pageList.isEmpty || pageNum == 0) {
+        final response = await http.get(api.getThreadListUri(boardName),
+            headers: {'Accept': 'application/json'});
+        if (response.statusCode == 200) {
+          pageList = jsonDecode(response.body) as List<dynamic>;
+          // threads = await compute(parseThreads, response.body);
+        } else {
+          return Future.error('Failed to fetch page');
+        }
       }
-      return threads;
+      final threadList = getThreadList(pageList, pageNum);
+      return Future.wait(
+          threadList.map((id) async => await fetchPost(boardName, id)));
     } catch (e) {
       return Future.error(e.toString());
     }
