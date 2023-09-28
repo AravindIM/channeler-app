89baf78d068affe411aa3c8ed4b5d6e76b158334
Aravind I M <aravindim@yahoo.com>
2023-09-28 15:13:36 +0530

    feat(backend): new algorithm to work with paginator

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -1,82 +1,96 @@
 import 'dart:convert';
 
-import 'package:channeler/backend/api_endpoint.dart';
 import 'package:channeler/backend/board.dart';
+import 'package:channeler/backend/paginator.dart';
 import 'package:channeler/backend/post.dart';
+import 'package:channeler/backend/session.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-class Backend {
-  List<Board> boards = [];
-  List<dynamic> pageList = [];
-  final ApiEndpoint api = ApiEndpoint.for4chan();
-
-  Future<List<Board>> fetchBoards() async {
-    try {
-      if (boards.isEmpty) {
-        final response = await http
-            .get(api.getBoardsUri(), headers: {'Accept': 'application/json'});
-        if (response.statusCode == 200) {
-          boards = await compute(parseBoards, response.body);
-        } else {
-          return Future.error('Failed to fetch boards');
-        }
-      }
-      return boards;
-    } catch (e) {
-      return Future.error(e.toString());
+Future<List<Board>> fetchBoards(Session session) async {
+  List<Board> boards;
+  try {
+    final response = await http.get(session.api.getBoardsUri(),
+        headers: {'Accept': 'application/json'});
+    if (response.statusCode == 200) {
+      boards = await compute(parseBoards, response.body);
+    } else {
+      return Future.error('Failed to fetch boards');
     }
+    return boards;
+  } catch (e) {
+    return Future.error(e.toString());
   }
+}
 
-  Board findBoardByName(String name) {
-    final board = boards.firstWhere((board) => board.name == name);
-    return board;
-  }
+Board findBoardByName(List<Board> boards, String name) {
+  final board = boards.firstWhere((board) => board.name == name);
+  return board;
+}
 
-  Future<Post> fetchPost(String boardName, int id) async {
-    try {
-      final response = await http.get(api.getThreadUri(boardName, id),
-          headers: {'Accept': 'application/json'});
-      if (response.statusCode == 200) {
-        final thread = jsonDecode(response.body) as Map<String, dynamic>;
-        return compute(Post.fromThreadJson, thread);
-      } else {
-        return Future.error('Failed to fetch thread');
-      }
-    } catch (e) {
-      return Future.error(e.toString());
+Future<Post> fetchPost(Session session, String boardName, int id) async {
+  try {
+    final response = await http.get(session.api.getThreadUri(boardName, id),
+        headers: {'Accept': 'application/json'});
+    if (response.statusCode == 200) {
+      final thread = jsonDecode(response.body) as Map<String, dynamic>;
+      return compute(Post.fromThreadJson, thread);
+    } else {
+      return Future.error('Failed to fetch thread');
     }
+  } catch (e) {
+    return Future.error(e.toString());
   }
+}
 
-  List<int> getThreadList(List<dynamic> pages, int pageNum) {
-    pageNum--;
-
-    final page = pages[pageNum] as Map<String, dynamic>;
-    final threadList = page['threads'] as List<dynamic>;
-
-    return threadList.map((json) {
-      final entry = json as Map<String, dynamic>;
-      return entry['no'] as int;
-    }).toList();
+Future<void> refreshPaginator(Session session, Paginator paginator) async {
+  if (paginator is CatalogPaginator) {
+    await refreshCatalogPaginator(session, paginator);
+  } else if (paginator is ThreadPaginator) {
+    await refreshThreadPaginator(session, paginator);
   }
+}
 
-  Future<List<Post>> fetchPage(String boardName, int pageNum) async {
-    try {
-      if (pageList.isEmpty || pageNum == 1) {
-        final response = await http.get(api.getThreadListUri(boardName),
-            headers: {'Accept': 'application/json'});
-        if (response.statusCode == 200) {
-          pageList = jsonDecode(response.body) as List<dynamic>;
-          // threads = await compute(parseThreads, response.body);
-        } else {
-          return Future.error('Failed to fetch page');
-        }
-      }
-      final threadList = getThreadList(pageList, pageNum);
-      return Future.wait(
-          threadList.map((id) async => await fetchPost(boardName, id)));
-    } catch (e) {
-      return Future.error(e.toString());
+Future<void> refreshCatalogPaginator(
+    Session session, CatalogPaginator paginator) async {
+  try {
+    final response = await http.get(
+        session.api.getThreadListUri(paginator.boardName),
+        headers: {'Accept': 'application/json'});
+    if (response.statusCode == 200) {
+      final threadListJson = jsonDecode(response.body) as List<dynamic>;
+      paginator.refreshFromThreadListJson(threadListJson);
+    } else {
+      return Future.error('Failed to fetch paginator');
     }
+  } catch (e) {
+    return Future.error(e.toString());
   }
+}
+
+Future<void> refreshThreadPaginator(
+    Session session, ThreadPaginator paginator) async {
+  try {
+    final response = await http.get(
+        session.api.getThreadUri(paginator.boardName, paginator.id),
+        headers: {'Accept': 'application/json'});
+    if (response.statusCode == 200) {
+      final threadJson = jsonDecode(response.body) as Map<String, dynamic>;
+      paginator.refreshFromThreadJson(threadJson);
+    } else {
+      return Future.error('Failed to fetch paginator');
+    }
+  } catch (e) {
+    return Future.error(e.toString());
+  }
+}
+
+Future<List<Post>> fetchPage(
+    Session session, Paginator paginator, int pageNum) async {
+  if (pageNum == 1) {
+    await refreshPaginator(session, paginator);
+  }
+  List<int> threads = paginator.getPage(pageNum);
+  return Future.wait(threads.map(
+      (thread) async => fetchPost(session, paginator.getBoardName(), thread)));
 }
