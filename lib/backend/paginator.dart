b75ddd212f7d0131edfe08f600b6e97b8956c662
Aravind I M <aravindim@yahoo.com>
2023-09-30 10:54:41 +0530

    feat(paginator): stream based paginator

--- lib/backend/paginator.dart
+++ lib/backend/paginator.dart
@@ -1,35 +1,45 @@
+import 'package:channeler/backend/post.dart';
+import 'package:flutter/foundation.dart';
+
 abstract interface class Paginator {
   String getBoardName();
-  int getMaxPages();
-  List<int> getPage(int pageNum);
+  int getPageSize();
+  Future<List<Post>> getNextPage();
 }
 
 class CatalogPaginator implements Paginator {
   final String boardName;
-  List<int> threads = [];
+  Stream<Post> pages = const Stream<Post>.empty().asBroadcastStream();
   int pageSize = 10;
 
   CatalogPaginator({required this.boardName});
 
-  void refreshFromThreadListJson(List<dynamic> threadListJson) {
-    threads = threadListJson
-        .map((pageJson) {
-          final page = pageJson as Map<String, dynamic>;
-          final pageThreads = page['threads'] as List<dynamic>;
-          return pageThreads.map((json) {
-            final entry = json as Map<String, dynamic>;
-            return entry['no'] as int;
-          });
-        })
-        .toList()
-        .expand((element) => element)
-        .toList();
+  Stream<Post> generatePosts(List<dynamic> catalogJson) async* {
+    final List<dynamic> threadList = await compute(
+        (_) => catalogJson
+            .map((pageJson) {
+              final page = pageJson as Map<String, dynamic>;
+              final threadList = page['threads'] as List<dynamic>;
+              return threadList;
+            })
+            .expand((thread) => thread)
+            .toList(),
+        "");
+    final List<Post> postList = await compute(
+        (_) => threadList.map((postJson) => Post.fromJson(postJson)).toList(),
+        "");
+    for (var post in postList) {
+      yield post;
+    }
+  }
+
+  void refreshFromCatalogJson(List<dynamic> catalogJson) {
+    pages = generatePosts(catalogJson).asBroadcastStream();
   }
 
   @override
-  int getMaxPages() {
-    var pagecount = threads.length / pageSize;
-    return pagecount.ceil();
+  Future<List<Post>> getNextPage() async {
+    return pages.take(pageSize).toList();
   }
 
   @override
@@ -38,31 +48,36 @@ class CatalogPaginator implements Paginator {
   }
 
   @override
-  List<int> getPage(int pageNum) {
-    final start = (pageNum - 1) * pageSize;
-    int end = start + pageSize;
-    if (end > threads.length) {
-      end = threads.length;
-    }
-
-    return threads.sublist(start, end);
+  int getPageSize() {
+    return pageSize;
   }
 }
 
 class ThreadPaginator implements Paginator {
   final String boardName;
   final int id;
-  List<int> threads = [];
+  Stream<Post> pages = const Stream<Post>.empty().asBroadcastStream();
   int pageSize = 10;
 
   ThreadPaginator({required this.boardName, required this.id});
 
-  void refreshFromThreadJson(Map<String, dynamic> threadJson) {
-    List<dynamic> posts = threadJson['posts'] as List<dynamic>;
-    threads = posts.map((postJson) {
-      final post = postJson as Map<String, dynamic>;
-      return post['no'] as int;
-    }).toList();
+  Stream<Post> generatePosts(Map<String, dynamic> threadJson) async* {
+    List<dynamic> postsJson = threadJson['posts'] as List<dynamic>;
+    final List<Post> postList = await compute(
+        (_) => postsJson.map((postJson) => Post.fromJson(postJson)).toList(),
+        "");
+    for (var post in postList) {
+      yield post;
+    }
+  }
+
+  void refreshFromThreadJson(Map<String, dynamic> catalogJson) {
+    pages = generatePosts(catalogJson).asBroadcastStream();
+  }
+
+  @override
+  Future<List<Post>> getNextPage() async {
+    return pages.take(pageSize).toList();
   }
 
   @override
@@ -71,19 +86,7 @@ class ThreadPaginator implements Paginator {
   }
 
   @override
-  int getMaxPages() {
-    var pagecount = threads.length / pageSize;
-    return pagecount.floor();
-  }
-
-  @override
-  List<int> getPage(int pageNum) {
-    final start = (pageNum - 1) * pageSize;
-    int end = start + pageSize;
-    if (end > threads.length) {
-      end = threads.length;
-    }
-
-    return threads.sublist(start, end);
+  int getPageSize() {
+    return pageSize;
   }
 }
