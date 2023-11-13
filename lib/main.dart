85c0c5509f4adb62acf2ecbc6c22d32dcf047566
Aravind I M <aravindim@yahoo.com>
2023-11-13 13:16:45 +0530

    Add: flex color scheme based light and dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:channeler/backend/api_endpoint.dart';
 import 'package:channeler/backend/session.dart';
 import 'package:channeler/router.dart';
 import 'package:fast_cached_network_image/fast_cached_network_image.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -18,33 +19,110 @@ void main() async {
   runApp(
     Provider<Session>(
       create: (_) => Session(api: ApiEndpoint.for4chan()),
-      child: const MyApp(),
+      child: MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final lightTheme = FlexThemeData.light(
+    scheme: FlexScheme.greenM3,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: false,
+      tintedDisabledControls: false,
+      blendOnColors: false,
+      useTextTheme: true,
+      inputDecoratorBorderType: FlexInputBorderType.underline,
+      inputDecoratorUnfocusedBorderIsColored: false,
+      alignedDropdown: true,
+      tooltipRadius: 4,
+      tooltipSchemeColor: SchemeColor.inverseSurface,
+      tooltipOpacity: 0.9,
+      useInputDecoratorThemeInDialogs: true,
+      snackBarElevation: 6,
+      snackBarBackgroundSchemeColor: SchemeColor.inverseSurface,
+      navigationBarSelectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationBarUnselectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationBarMutedUnselectedLabel: false,
+      navigationBarSelectedIconSchemeColor: SchemeColor.onSurface,
+      navigationBarUnselectedIconSchemeColor: SchemeColor.onSurface,
+      navigationBarMutedUnselectedIcon: false,
+      navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
+      navigationBarIndicatorOpacity: 1.00,
+      navigationRailSelectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationRailUnselectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationRailMutedUnselectedLabel: false,
+      navigationRailSelectedIconSchemeColor: SchemeColor.onSurface,
+      navigationRailUnselectedIconSchemeColor: SchemeColor.onSurface,
+      navigationRailMutedUnselectedIcon: false,
+      navigationRailIndicatorSchemeColor: SchemeColor.secondaryContainer,
+      navigationRailIndicatorOpacity: 1.00,
+      navigationRailBackgroundSchemeColor: SchemeColor.surface,
+      navigationRailLabelType: NavigationRailLabelType.none,
+    ),
+    keyColors: const FlexKeyColors(),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    useMaterial3: true,
+    swapLegacyOnMaterial3: true,
+    // To use the Playground font, add GoogleFonts package and uncomment
+    // fontFamily: GoogleFonts.notoSans().fontFamily,
+  );
+
+  final darkTheme = FlexThemeData.dark(
+    scheme: FlexScheme.greenM3,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: false,
+      tintedDisabledControls: false,
+      useTextTheme: true,
+      inputDecoratorBorderType: FlexInputBorderType.underline,
+      inputDecoratorUnfocusedBorderIsColored: false,
+      alignedDropdown: true,
+      tooltipRadius: 4,
+      tooltipSchemeColor: SchemeColor.inverseSurface,
+      tooltipOpacity: 0.9,
+      useInputDecoratorThemeInDialogs: true,
+      snackBarElevation: 6,
+      snackBarBackgroundSchemeColor: SchemeColor.inverseSurface,
+      navigationBarSelectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationBarUnselectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationBarMutedUnselectedLabel: false,
+      navigationBarSelectedIconSchemeColor: SchemeColor.onSurface,
+      navigationBarUnselectedIconSchemeColor: SchemeColor.onSurface,
+      navigationBarMutedUnselectedIcon: false,
+      navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
+      navigationBarIndicatorOpacity: 1.00,
+      navigationRailSelectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationRailUnselectedLabelSchemeColor: SchemeColor.onSurface,
+      navigationRailMutedUnselectedLabel: false,
+      navigationRailSelectedIconSchemeColor: SchemeColor.onSurface,
+      navigationRailUnselectedIconSchemeColor: SchemeColor.onSurface,
+      navigationRailMutedUnselectedIcon: false,
+      navigationRailIndicatorSchemeColor: SchemeColor.secondaryContainer,
+      navigationRailIndicatorOpacity: 1.00,
+      navigationRailBackgroundSchemeColor: SchemeColor.surface,
+      navigationRailLabelType: NavigationRailLabelType.none,
+    ),
+    keyColors: const FlexKeyColors(),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    useMaterial3: true,
+    swapLegacyOnMaterial3: true,
+    // To use the Playground font, add GoogleFonts package and uncomment
+    // fontFamily: GoogleFonts.notoSans().fontFamily,
+  );
+// If you do not have a themeMode switch, uncomment this line
+// to let the device system mode control the theme mode:
+// themeMode: ThemeMode.system,
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Channeler',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)
-            .copyWith(background: Colors.white, onBackground: Colors.black),
-        useMaterial3: true,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.green),
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-          ),
-        ),
-        listTileTheme: ListTileThemeData(
-          selectedTileColor: Colors.green.shade100,
-          selectedColor: Colors.black,
-        ),
-      ),
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
       routerConfig: appRouter,
     );
   }
